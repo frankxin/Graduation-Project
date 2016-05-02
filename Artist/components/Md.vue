8371c39ffe4b1ec49c79018d8438d1a701de5f27
frankxin <frankxin93@gmail.com>
2016-05-03 01:01:04 +0800

    change jquery to zepto.js and fix some bugs

--- Artist/components/Md.vue
+++ Artist/components/Md.vue
@@ -12,28 +12,45 @@
 		},
 		created: function(){
 			this.load().then(() => {
+				//内容的html从后端获取，获取后才能绑定事件
 				this.bindEvent()
 			})
 		},
 		methods: {
 			bindEvent: function (){
-				document.onkeydown = function(){
-					alert('love you')
+				var $slide = $('.slide'),
+					 $slides = $('#slides')
+				$slide.eq(0).addClass('now')
+				$slide.eq(1).addClass('next')
+				document.onkeydown = function(e){
+					console.log(e.which)
+					if(e.keyCode === 39){
+						if($slides.find('.now').next().length !== 0){
+							$slides.find('.now').removeClass('now').addClass('prev').prev().removeClass('prev')
+						  $slides.find('.next').removeClass('next').addClass('now').next().addClass('next')
+						}
+					}else if(e.keyCode === 37){
+						if($slides.find('.now').prev().length !== 0){
+							$slides.find('.now').removeClass('now').addClass('next').prev().removeClass('prev').addClass('now')
+							$slides.find('.next').removeClass('.next')
+						}
+					}else{
+						return false
+					}
 				}
 			},
 			load: function (){
 				return new Promise((resolve,reject) => {
+					// zepto of this version don't have  callback module , so you can't use xhr.done()...
 					$.ajax({
 						url: "http://127.0.0.1:3000/md/demo",
 						method: 'GET',
 						dataType: 'jsonp',
-						headers: {
-							"Access-Control-Allow-Origin" : "*"
+						success: (data) => {
+							this.message = data.content
+						  resolve()
 						}
-					}).done(function(data){
-						this.message = data.content
-						resolve()
-					}.bind(this))
+					})
 				})
 			}
 		}
